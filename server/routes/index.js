5bc5b3a9d755d961d12b980088f8e80ca2359a1c
clearives <clearives@gmail.com>
2018-11-14 15:40:30 +0800

    微信分享接口

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,9 @@
 const router = require('koa-router')()
 const { serverVerify } = require('../controllers/serverVerify')
+const { wxJsConfig } = require('../controllers/iwx/wxJsConfig')
 
 router.get('/serverVerify', serverVerify)
+router.get('/iwx/wxJsConfig', wxJsConfig)
 
 
 module.exports = router

3fa97dabea74e5b120cd1614d968038694cbe35a
clearives <clearives@gmail.com>
2018-11-19 14:49:49 +0800

    微信分享测试OK

--- client/src/views/MainView.jsx
+++ client/src/views/MainView.jsx
@@ -1,23 +1,28 @@
 import React, {Component} from 'react'
 import './MainView.css'
+import wxShare from '../utils/wxShare'
 
+wxShare({
+  title: '一二三四五',
+  desc: '上山打老虎',
+  link: location.href,
+  imgUrl: 'http://pc1g4qy0i.bkt.clouddn.com//upload/QQ20181114-162917@2x.png'
+})
 export default class MainView extends Component {
-  static propTypes = {
+  static propTypes = {};
 
-  };
   constructor (props) {
     super(props)
-    this.state = {
-
-    }
+    this.state = {}
   }
+
   render () {
     return (
       <div className='main-view'>
         <div className='App-header'>
-          <img src={require('../assets/logo.jpg')} className='App-logo shake-rotate' alt='logo' />
+          <img src={`http://pc1g4qy0i.bkt.clouddn.com//upload/QQ20181114-162917@2x.png`} className='App-logo shake-rotate' alt='logo' />
         </div>
-        <h3 className='App-title'>cc-wehcat</h3>
+        <h3 className='App-title'>鹏鹏，你试试~~~</h3>
         <a className='href' href='#/app'>app</a>
       </div>
     )

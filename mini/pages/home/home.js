02cd82334660ad7b54e9eddd9fb215d6e0b0d7e4
clearives <clearives@gmail.com>
2018-11-22 15:02:43 +0800

    调用接口查询ip地址

--- mini/pages/home/home.js
+++ mini/pages/home/home.js
@@ -1,5 +1,13 @@
+import Http from '../../utils/http'
 Page({
   data: {
-    
+    address: ''
+  },
+  onLoad: function() {
+    Http.get('http://api.map.baidu.com/location/ip?ak=F454f8a5efe5e577997931cc01de3974', {}).then(res => {
+      this.setData({
+        address: res.data.address
+      })
+    })
   }
 })

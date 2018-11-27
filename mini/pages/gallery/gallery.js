bbdd91554fb65baf06e5946e5fb38ceb38d9a570
clearives <clearives@gmail.com>
2018-11-27 14:42:37 +0800

    详情页新增头部滑动菜单

--- mini/pages/gallery/gallery.js
+++ mini/pages/gallery/gallery.js
@@ -8,13 +8,20 @@ Page({
   data: {
     id: '',
     title: '',
-    categoryData: []
+    category: [],
+    categoryData: [],
+    imgUrls: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    Http.get('http://service.picasso.adesk.com/v1/vertical/category?adult=false&first=1', {}).then(res => {
+      this.setData({
+        category: res.data.res.category
+      })
+    })
     console.log(options)
     this.setData({
       id: options.id,
@@ -76,14 +83,43 @@ Page({
 
   },
 
+  tabGalleryDetail: function(e) {
+    var item = e.currentTarget.dataset.item
+    this.setData({
+      id: item.id,
+      title: item.name
+    })
+    wx.setNavigationBarTitle({
+      title: item.name,
+    })
+    this.getGalleryDetail(item.id)
+  },
+
   getGalleryDetail: function(id) {
     let cid = id || this.data.id
     let url = !cid ? "http://service.picasso.adesk.com/v1/vertical/vertical?limit=30&skip=180&adult=false&first=0&order=hot" :
       "http://service.picasso.adesk.com/v1/vertical/category/" + cid + "/vertical?limit=30&adult=false&first=1&order=new"
     Http.get(url, {}).then(res => {
       this.setData({
-        categoryData: res.data.res.vertical
+        categoryData: res.data.res.vertical,
+        imgUrls: this.getUrls(res.data.res.vertical)
       })
+    })
+  },
+
+  getUrls: function (data) {
+    let _arr = []
+    data.map((v, k) => {
+      _arr.push(v.img)
+    })
+    return _arr
+  },
+
+  previewImage: function (e) {
+    var current = e.target.dataset.item
+    wx.previewImage({
+      current: current.img, 
+      urls: this.data.imgUrls  
     })
   }
 })

916642af53fb33bfa0ed88dcbcf2b74fbff8ea0a
clearives <clearives@gmail.com>
2018-11-28 14:56:48 +0800

    个人中心布局调整

--- mini/pages/me/me.js
+++ mini/pages/me/me.js
@@ -1,14 +1,11 @@
 const app = getApp()
-const { _Toast } = require('../../components/base/index');
 
 Page({
   data: {
     userInfo: {}
   },
   onLoad: () => {
-    _Toast({
-      content: '文本提示'
-    });
+   
   },
   onShow: function () {
     console.log(this)

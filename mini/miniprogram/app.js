7e94d7c8daf9fe973237014ccfb2d8df7d08cf1c
clearives <clearives@gmail.com>
2018-11-29 16:42:36 +0800

    test api

--- mini/miniprogram/app.js
+++ mini/miniprogram/app.js
@@ -5,15 +5,25 @@ App({
     } else {
       wx.cloud.init()
     }
-    wx.cloud.callFunction({
-        // 云函数名称
-        name: 'getUser',
-        // 传给云函数的参数
-        data: {},
-      })
-      .then(res => {
-        console.log(res.result)
-      })
-      .catch(console.error)
+    // wx.cloud.callFunction({
+    //   // 云函数名称
+    //   name: 'getUser',
+    //   // 传给云函数的参数
+    //   data: {},
+    // }).then(res => {
+    //   console.log(res.result)
+    // }).catch(console.error)
+
+    const db = wx.cloud.database()
+    // db.collection('user').add({
+    //   data: {
+    //     name: 'clearives'
+    //   }
+    // }).then(res => {
+    //   console.log(res)
+    // }).catch(console.error)
+    db.collection('user').where({}).get().then(res => {
+      console.log(res.data)
+    })
   }
 })

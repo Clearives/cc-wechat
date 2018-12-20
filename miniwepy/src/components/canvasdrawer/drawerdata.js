bb988c4e76153d6a69077fdb8c9f153c498d29b4
clearives <clearives@gmail.com>
2018-12-20 11:11:36 +0800

    绘制图片

---
+++ miniwepy/src/components/canvasdrawer/drawerdata.js
@@ -0,0 +1,109 @@
+export default {
+  "width": 375,
+  "height": 600,
+  "clear": true,
+  "views": [
+    {
+      "type": "roundRect",
+      "x": 0,
+      "y": 0,
+      "width": 375,
+      "height": 600,
+      "radius": 5,
+      "background": '#0085fc'
+    },
+    {
+      "type": "roundRect",
+      "x": 25,
+      "y": 25,
+      "width": 325,
+      "height": 540,
+      "radius": 10,
+      "background": '#fff'
+    },
+    {
+      "type": "image",
+      "url": "http://pjx5tui9j.bkt.clouddn.com/superbed/2018/12/18/5c18a063c4ff9e2b4d698d0c.jpg",
+      "top": 35,
+      "left": 50,
+      "width": 55,
+      "height": 55,
+      "borderRadius": 28,
+      "isCircle": true
+    },
+    {
+      "type": "image",
+      "url": "http://pjx5tui9j.bkt.clouddn.com/superbed/2018/12/19/5c19b56bc4ff9e2b4d698e34.jpg",
+      "top": 110,
+      "left": 50,
+      "width": 275,
+      "height": 275,
+      "borderRadius": Math.floor(275/2),
+      "isCircle": true
+    },
+    {
+      "type": "image",
+      "url": "http://pjx5tui9j.bkt.clouddn.com/superbed/2018/12/19/5c19b834c4ff9e2b4d698e3f.jpg",
+      "top": 460,
+      "left": 225,
+      "width": 80,
+      "height": 80
+    },
+    {
+      "type": "text",
+      "content": "您的好友【Clearives】",
+      "fontSize": 16,
+      "color": "#000",
+      "textAlign": "left",
+      "top": 38,
+      "left": 118,
+      "bolder": false
+    },
+    {
+      "type": "text",
+      "content": "https://github.com/clearives",
+      "fontSize": 14,
+      "color": "#a0a3a8",
+      "textAlign": "left",
+      "top": 65,
+      "left": 118
+    },
+    {
+      "type": "text",
+      "content": "[鸡汤]最好的未来是我们一起对抗平庸，一起披荆斩棘，一起克服柴米油盐，一起面对生老病死，你不讨厌我，我也依然爱着你",
+      "fontSize": 16,
+      "lineHeight": 24,
+      "color": "#383549",
+      "textAlign": "left",
+      "top": 400,
+      "left": 50,
+      "width": 260,
+      "MaxLineNumber": 2,
+      "breakWord": true,
+      "bolder": true
+    },
+    {
+      "type": "text",
+      "content": "长按识别图中二维码~",
+      "fontSize": 14,
+      "color": "#383549",
+      "textAlign": "left",
+      "top": 490,
+      "left": 70,
+      "lineHeight": 20,
+      "MaxLineNumber": 2,
+      "breakWord": true,
+      "width": 130
+    },
+    {
+      "type": "text",
+      "content": "果粒奶优不好喝",
+      "fontSize": 12,
+      "color": "#f8f8f8",
+      "textAlign": "center",
+      "top": 570,
+      "left": 375/2,
+      "width": 375,
+    },
+  ]
+};

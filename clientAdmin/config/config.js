cc9d2e7bc4f9135b2cd841e6843ab9ba311d29f6
clearives <clearives@gmail.com>
2018-12-28 11:43:53 +0800

    配置调整

--- clientAdmin/config/config.js
+++ clientAdmin/config/config.js
@@ -60,13 +60,13 @@ export default {
   externals: {
     '@antv/data-set': 'DataSet',
   },
-  // proxy: {
-  //   '/server/api/': {
-  //     target: 'https://preview.pro.ant.design/',
-  //     changeOrigin: true,
-  //     pathRewrite: { '^/server': '' },
-  //   },
-  // },
+  proxy: {
+    "/api": {
+      "target": "",
+      "changeOrigin": true,
+      "pathRewrite": { "^/api" : "/" }
+    }
+  },
   ignoreMomentLocale: true,
   lessLoaderOptions: {
     javascriptEnabled: true,
--- clientAdmin/config/router.config.js
+++ clientAdmin/config/router.config.js
@@ -5,22 +5,46 @@ export default [
     routes: [
       {
         path: '/',
-        redirect: '/product/income',
+        redirect: '/product/intro',
       },
       {
-        path: '/product/income',
-        component: './product/income',
-        name: 'analysis',
+        path: '/product',
+        name: '项目',
+        icon: 'dashboard',
+        routes: [
+          {
+            path: '/product/intro',
+            component: './product/intro',
+            name: '介绍',
+          },
+          {
+            path: '/product/income',
+            component: './product/income',
+            name: '管理',
+          },
+          {
+            path: '/product/list',
+            component: './product/list',
+            name: '列表',
+          },
+        ]
       },
       {
-        path: '/product/inout',
-        component: './product/inout',
-        name: 'inout',
-      },
-      {
-        path: '/product/list',
-        component: './product/list',
-        name: 'monitor',
+        path: '/github',
+        name: 'Github',
+        icon: 'github',
+        routes: [
+          {
+            path: '/github/repo',
+            component: './product/income',
+            name: '仓库',
+          },
+          {
+            path: '/github/issue',
+            component: './product/intro',
+            name: '文章',
+          },
+        ]
       },
     ],
   },
